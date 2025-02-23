5ac4067c62a61d2766efc8ca314c4abb216f62f4
Jalil Artykov <artykovjalil@gmail.com>
2025-02-24 03:49:24 +0600

    beta 0.6

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,9 @@
 # Выполняем миграции
 python manage.py migrate
 
-# Запускаем Gunicorn (Django)
-gunicorn cargo.wsgi:application --bind 0.0.0.0:8000 &
-
-# Запускаем Celery worker
+# Запускаем Celery worker и beat в фоне (они второстепенны)
 celery -A cargo worker --loglevel=info &
-
-# Запускаем Celery beat
 celery -A cargo beat --loglevel=info &
 
-# Ожидаем завершения всех процессов
-wait
+# Запускаем Gunicorn на переднем плане (основной процесс)
+exec gunicorn cargo.wsgi:application --bind 0.0.0.0:8000
